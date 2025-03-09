651fed2289957f394a1879aa2c6c0c7308915ef4
Aqua200 <chinquepapa@gmail.com>
2025-03-09 03:22:45 -0700

    Update tools-say.js

--- plugins/tools-say.js
+++ plugins/tools-say.js
@@ -1,18 +1,21 @@
 let handler = async (m, { conn, args }) => {
-    if (!args.length) return conn.sendMessage(m.chat, { text: `${emoji} Por favor, escribe el texto que deseas repetir.` });
-    let message = args.join(' ');
+    if (!args.length) return conn.sendMessage(m.chat, { text: '⚠️ Por favor, escribe el texto que deseas repetir.' });
 
+    let message = args.join(' ');
     let invisibleChar = '\u200B';
     let finalMessage = invisibleChar + message;
 
     let mentions = [...message.matchAll(/@(\d+)/g)].map(v => v[1] + '@s.whatsapp.net');
-    if (mentions.length) {
-        conn.sendMessage(m.chat, { text: finalMessage, mentions });
-    } else {
-        conn.sendMessage(m.chat, { text: finalMessage });
+
+    try {
+        await conn.sendMessage(m.chat, { text: finalMessage, mentions: mentions.length ? mentions : undefined });
+    } catch (e) {
+        console.error('Error enviando el mensaje:', e);
     }
 };
-handler.command = ['say', 'decir']
-handler.tag = ['tools'];
+
+handler.command = ['say', 'decir'];
+handler.tags = ['tools'];
 handler.group = true;
+
 export default handler;
