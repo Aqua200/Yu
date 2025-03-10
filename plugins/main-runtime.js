e188a8893c54550b35c7726317eb349a6a778b88
Aqua200 <chinquepapa@gmail.com>
2025-03-10 03:22:20 -0700

    Update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -1,28 +1,17 @@
-let handler = async (m, { conn, usedPrefix, command }) => {
-    let _uptime = process.uptime() * 1000
-    let tio = clockString(_uptime)
-    let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
-
-var ct = `
-*───「 RUNTIME BOT 」───*
-
-Runtime : ${tio}
-    `
-m.reply(ct)
+let handler = async (m) => {
+    let muptime = clockString(process.uptime() * 1000) // Convierte a milisegundos
+    m.reply(`🏮 *Tiempo activo:* \n\n${muptime}`)
 }
-handler.help = ['runtime']
-handler.tags = ['info']
-handler.command = /^(uptime|runtime)$/i
 
-module.exports = handler
+handler.help = ['runtime']
+handler.tags = ['main']
+handler.command = ['runtime', 'uptime']
+export default handler
 
 function clockString(ms) {
-    let days = Math.floor(ms / (24 * 60 * 60 * 1000));
-    let daysms = ms % (24 * 60 * 60 * 1000);
-    let hours = Math.floor((daysms) / (60 * 60 * 1000));
-    let hoursms = ms % (60 * 60 * 1000);
-    let minutes = Math.floor((hoursms) / (60 * 1000));
-    let minutesms = ms % (60 * 1000);
-    let sec = Math.floor((minutesms) / (1000));
-    return days + " Day " + hours + " Hour " + minutes + " Minute " + sec + " Second ";
+    let d = Math.floor(ms / 86400000)
+    let h = Math.floor(ms / 3600000) % 24
+    let m = Math.floor(ms / 60000) % 60
+    let s = Math.floor(ms / 1000) % 60
+    return `${d}d ${h}h ${m}m ${s}s`
 }
