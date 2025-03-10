8a54b8c28e317c94e4192778887b948eae5598c4
Aqua200 <chinquepapa@gmail.com>
2025-03-10 03:17:19 -0700

    Update and rename main-runtime.js to main-uptime.js

--- plugins/main-uptime.js
+++ plugins/main-uptime.js
@@ -2,12 +2,12 @@ let handler = async (m, { usedPrefix, command }) => {
   let uptime = await process.uptime();
   let imageUrl = 'https://qu.ax/HGPmS.jpeg';  // Reemplaza esta URL con la que quieras usar
 
-  let runtime = `
+  let mensaje = `
 ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
 🌸  Welcome ! 🌸
 ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
 
-⛩️【 Mi servidor  】⛩️
+⛩️【 Mi servidor 】⛩️
 ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
 
 🌷 *Fecha:* ${new Date().toLocaleDateString()}
@@ -21,7 +21,7 @@ let handler = async (m, { usedPrefix, command }) => {
 
 ╭━━━━━━━━━━━━━━━━━━━━━━━━━━━━╮
 
-🖤 *Gracias por estar aquí, ¡espero que tu día sea tan hermoso como la mia !* 🤍
+🖤 *Gracias por estar aquí, ¡espero que tu día sea tan hermoso como el mío!* 🤍
   
 💬 Si quieres saber más sobre el estado completo, escribe *${usedPrefix}status*.
 
@@ -29,16 +29,16 @@ let handler = async (m, { usedPrefix, command }) => {
 `;
 
   // Enviar la imagen junto con el mensaje
-  conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: runtime }, { quoted: m });
+  conn.sendMessage(m.chat, { image: { url: imageUrl }, caption: mensaje }, { quoted: m });
 };
 
-handler.help = ['runtime'];
+handler.help = ['tiempoactivo'];
 handler.tags = ['main'];
-handler.command = ['runtime', 'uptime'];
+handler.command = ['tiempoactivo', 'uptime'];
 
 export default handler;
 
-const dd = new Date(new Date + 3600000);
+const dd = new Date(new Date() + 3600000);
 const time = dd.toLocaleString('en-US', {
   hour: 'numeric',
   minute: 'numeric',
@@ -52,9 +52,9 @@ function rTime(seconds) {
   var h = Math.floor((seconds % (3600 * 24)) / 3600);
   var m = Math.floor((seconds % 3600) / 60);
   var s = Math.floor(seconds % 60);
-  var dDisplay = d > 0 ? d + (d == 1 ? " día, " : " Días, ") : "";
-  var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " Horas, ") : "";
-  var mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " Minutos, ") : "";
-  var sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " Segundos") : "";
+  var dDisplay = d > 0 ? d + (d == 1 ? " día, " : " días, ") : "";
+  var hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " horas, ") : "";
+  var mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " minutos, ") : "";
+  var sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " segundos") : "";
   return dDisplay + hDisplay + mDisplay + sDisplay;
 }
