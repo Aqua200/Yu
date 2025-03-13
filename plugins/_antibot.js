b4064dfff532988a42a43dc04db35f8550b927c3
Aqua200 <chinquepapa@gmail.com>
2025-03-12 22:01:54 -0700

    Update _antibot.js

--- plugins/_antibot.js
+++ plugins/_antibot.js
@@ -1,20 +1,24 @@
 export async function before(m, { conn, isAdmin, isBotAdmin }) {
     if (!m.isGroup) return;
-    let chat = global.db.data.chats[m.chat]
-    let delet = m.key.participant
-    let bang = m.key.id
-    let bot = global.db.data.settings[this.user.jid] || {}
+    
+    let chat = global.db.data.chats[m.chat];
+    let delet = m.key.participant;
+    let bang = m.key.id;
+    let bot = global.db.data.settings[this.user.jid] || {};
+    
     if (m.fromMe) return true;
 
+    // Detecta IDs de bot
     if (m.id.startsWith('3EB0') && m.id.length === 22) {
-        let chat = global.db.data.chats[m.chat];
-
         if (chat.antiBot) {
-       //     await conn.reply(m.chat, `     ͞ ͟͞ ͟${packname}͟͞ ͟ ͟͞ ͞   \n╚▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬▭╝\n\n𝑆𝑜𝑦 ${botname} 𝑙𝑎 𝑚𝑒𝑗𝑜𝑟 𝑏𝑜𝑡 𝑑𝑒 𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑!!\n𝐸𝑠𝑡𝑒 𝑔𝑟𝑢𝑝𝑜 𝑛𝑜 𝑡𝑒 𝑛𝑒𝑐𝑒𝑠𝑖𝑡𝑎, 𝑎𝑑𝑖𝑜𝑠𝑖𝑡𝑜 𝑏𝑜𝑡 𝑑𝑒 𝑠𝑒𝑔𝑢𝑛𝑑𝑎.`, m);
+            let nekoDecor = `〘 ~🤍 〙\n\n` + 
+                            `🌸 𝙎𝙤𝙮 *${botname}*, 𝙡𝙖 𝙗𝙤𝙩 𝙢á𝙨 𝙘𝙪𝙠𝙞~ 🖤\n` + 
+                            `🚫 𝙀𝙨𝙩𝙚 𝙜𝙧𝙪𝙥𝙤 𝙣𝙤 𝙩𝙚 𝙣𝙚𝙘𝙚𝙨𝙞𝙩𝙖, 𝙖𝙙𝙞𝙤𝙨𝙞𝙩𝙤 𝙗𝙤𝙩-𝙨𝙚𝙣𝙥𝙖𝙞! ✨`;
 
             if (isBotAdmin) {
-await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+                await conn.sendMessage(m.chat, { text: nekoDecor });
+                await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }});
+                await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
             }
         }
     }
