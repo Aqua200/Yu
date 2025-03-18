4eaedeabd3be782a9c35886a39bfae632ad35b7d
Aqua200 <chinquepapa@gmail.com>
2025-03-18 08:28:19 -0700

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,16 +1,26 @@
-conn.ev.on('group-participants.update', async (update) => {
-  const { id, participants, action } = update
-  for (const user of participants) {
-    let pp = await conn.profilePictureUrl(user, 'image').catch(() => 'https://files.catbox.moe/xr2m6u.jpg')
-    const img = await (await fetch(pp)).buffer()
-    const taguser = `@${user.split('@')[0]}`
-    const metadata = await conn.groupMetadata(id)
-    if (action === 'add') {
-      const bienvenida = `❀ *Bienvenido* a ${metadata.subject}\n✰ ${taguser}\n${global.welcom1}\n •(=^●ω●^=)• ¡Disfruta tu estadía!\n> ✐ Usa *#help* para comandos.`
-      await conn.sendMessage(id, { image: img, caption: bienvenida, mentions: [user] })
-    } else if (action === 'remove') {
-      const despedida = `❀ *Adiós* de ${metadata.subject}\n✰ ${taguser}\n${global.welcom2}\n •(=^●ω●^=)• ¡Te esperamos pronto!\n> ✐ Usa *#help* para comandos.`
-      await conn.sendMessage(id, { image: img, caption: despedida, mentions: [user] })
+import { WAMessageStubType } from '@whiskeysockets/baileys'
+import fetch from 'node-fetch'
+
+export async function before(m, { conn, participants, groupMetadata }) {
+  if (!m.messageStubType || !m.isGroup) return !0;
+
+  let who = m.messageStubParameters[0]
+  let taguser = `@${who.split('@')[0]}`
+  let chat = global.db.data.chats[m.chat]
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
+  let img = await (await fetch(`${pp}`)).buffer()
+
+    if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
+      let bienvenida = `❀ *Bienvenido* a ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom1}\n •(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
     }
-  }
-})
+       
+    if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
+      let bye = `❀ *Adiós* de ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom2}\n •(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
+    }
+
+    if (chat.welcome && m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE) { 
+      let kick = `❀ *Adiós* de ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom2}\n •(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+      await conn.sendMessage(m.chat, { image: img, caption: kick, mentions: [who] })
+  }}
