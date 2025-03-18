0eb437fb6c89a75d60c5f1c3cd9f307f85ef697f
Aqua200 <chinquepapa@gmail.com>
2025-03-17 22:41:50 -0700

    Update rgusuarios.js

--- plugins/rgusuarios.js
+++ plugins/rgusuarios.js
@@ -3,10 +3,12 @@ import db from '../lib/database.js'
 let handler = async function (m, { conn }) {
   const users = global.db.data.users
   const totalRegistrados = Object.values(users).filter(u => u.registered === true).length
+  const totalreg = Object.keys(users).length  // Total de usuarios en la base
 
   let message = `♡✧  𝗧𝗢𝗧𝗔𝗟 𝗗𝗘 𝗨𝗦𝗨𝗔𝗥𝗜𝗢𝗦 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢𝗦  ✧♡\n`
   message += `╭───────────────╮\n`
-  message += `🌸 Total: *${totalRegistrados}* usuarios\n`
+  message += `🌸 Total registrados: *${totalRegistrados}* usuarios\n`
+  message += `🌼 Total en la base: *${totalreg}* usuarios\n`
   message += `╰───────────────╯\n`
   message += `Gracias por confiar en 2B.`
 
