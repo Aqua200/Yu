5f398ea166c34cfa370a77fa26f960e71a5cc944
Aqua200 <chinquepapa@gmail.com>
2025-03-20 20:12:39 -0700

    Update Script.js

--- plugins/Script.js
+++ plugins/Script.js
@@ -77,7 +77,7 @@ ${leerMas}
 »⊹˚୨ *2B* ⊹
 `.trim()
 
-    let pp = 'https://files.catbox.moe/rexl71.jpg' // Cambia por tu imagen
+    let pp = 'https://files.catbox.moe/58o60y.jpg' // Cambia por tu imagen
     await conn.sendFile(m.chat, pp, 'thumbnail.jpg', menuText, m)
 
   } catch (e) {
