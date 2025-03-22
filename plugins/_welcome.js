5b96d2f6c3988ff4901d0aa87f16d5fb22b3677c
Aqua200 <chinquepapa@gmail.com>
2025-03-22 09:32:59 -0700

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -7,25 +7,41 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
-  let defaultImage = 'https://files.catbox.moe/xr2m6u.jpg';
+  let defaultImage = 'https://files.catbox.moe/xr2m6u.jpg'
 
   if (chat.welcome) {
-    let img;
+    let img
     try {
-      let pp = await conn.profilePictureUrl(who, 'image');
-      img = await (await fetch(pp)).buffer();
+      let pp = await conn.profilePictureUrl(who, 'image')
+      img = await (await fetch(pp)).buffer()
     } catch {
-      img = await (await fetch(defaultImage)).buffer();
+      img = await (await fetch(defaultImage)).buffer()
     }
 
+    let cantidad = participants.length
+
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-      let bienvenida = `🍨 *Bienvenido* a ${groupMetadata.subject}\n 「 ${taguser}\n${global.welcom1}\n •Ｏ(≧∇≦)Ｏ• Disfruta tu estadía en el grupo!\n> 🍡 Puedes usar *#help* para ver la lista de comandos.`
-      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
+      let bienvenida = `
+╭─▸ 𖥻 Bienvenida a *${groupMetadata.subject}*  
+│ Nombre: ${taguser}  
+│  
+│ ${global.welcom1}  
+│  
+│ Actualmente somos *${cantidad}* integrantes  
+╰─────────────▸  
+Comandos disponibles: *#help*`
+      await conn.sendMessage(m.chat, { image: img, caption: bienvenida.trim(), mentions: [who] })
     } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
-      let bye = `💐 *│「 𝐀𝐃𝐈Ó𝐒 🗣️‼️ 」* De ${groupMetadata.subject}\n  」 ${taguser}\n${global.welcom2}\n │😒  𝐒𝐄 𝐅𝐔𝐄 𝐄𝐒𝐄 𝐏𝐔𝐓𝐎
-   │🥀 𝐍𝐮𝐧𝐜𝐚 𝐓𝐞 𝐐𝐮𝐢𝐬𝐢𝐦𝐨𝐬 𝐀𝐪𝐮í
-   └───────────────┈ ⳹ \n> 💐 Puedes usar *#help* para ver la lista de comandos.`
-      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
+      let despedida = `
+╭─▸ 𖥻 Despedida de *${groupMetadata.subject}*  
+│ Usuario: ${taguser}  
+│  
+│ ${global.welcom2}  
+│  
+│ Se fue uno, ahora somos *${cantidad - 1}*  
+╰─────────────▸  
+Comandos disponibles: *#help*`
+      await conn.sendMessage(m.chat, { image: img, caption: despedida.trim(), mentions: [who] })
     }
   }
 
