4f6f37551a79411e20a87d8f394ba66a38135750
Aqua200 <chinquepapa@gmail.com>
2025-03-24 16:53:03 -0700

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,28 +1,29 @@
 import fetch from 'node-fetch'
 
-let handler  = async (m, { conn, usedPrefix, command }) => {
+let handler = async (m, { conn, usedPrefix, command }) => {
 
-let grupos = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad.....*
+let grupos = `
+*Ven a unirte a los grupos oficiales de y comparte momentos lindos con la comunidad:*
 
-- ${namegrupo}
-> *❀* ${gp1}
+❥ *${namegrupo}*
+> ✿ ${gp1}
 
-${namecomu}
-> *❀* ${comunidad1}
+❥ *${namecomu}*
+> ✿ ${comunidad1}
 
-*ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ*
+*⌒☆⌒☆⌒☆⌒☆⌒☆⌒☆⌒☆⌒☆*
 
-⚘ Enlace anulado? entre aquí! 
+⚘ ¿El enlace no funciona? No te preocupes, entra aquí:
 
-- ${namechannel}
-> *❀* ${channel}
+❥ *${namechannel}*
+> ✿ ${channel}
 
-- ${namechannel2}
-> *❀* ${channel2}
+❥ *${namechannel2}*
+> ✿ ${channel2}
 
-> ${dev}`
+> *Con cariño, tu Neko dev* ${dev}
+`
 
-// Coloca la URL de la imagen aquí:
 const imageUrl = 'https://files.catbox.moe/jyaacp.jpg'
 
 await conn.sendFile(m.chat, imageUrl, "grupos.jpg", grupos, m)
@@ -30,6 +31,7 @@ await conn.sendFile(m.chat, imageUrl, "grupos.jpg", grupos, m)
 await m.react(emojis)
 
 }
+
 handler.help = ['grupos']
 handler.tags = ['info']
 handler.command = ['grupos', 'links', 'groups']
