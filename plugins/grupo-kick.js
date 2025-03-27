f6fe512a86f012cbd5f535b9748dae1844d15a5c
Aqua200 <chinquepapa@gmail.com>
2025-03-27 14:28:09 -0700

    Update grupo-kick.js

--- plugins/grupo-kick.js
+++ plugins/grupo-kick.js
@@ -1,38 +1,29 @@
-var handler = async (m, { conn, participants, usedPrefix, command }) => {
+var handler = async (m, { conn }) => {
     if (!m.mentionedJid[0] && !m.quoted) {
-        return conn.reply(m.chat, `${emoji} Debes mencionar a un usuario para poder expulsarlo del grupo.`, m);
+        return conn.reply(m.chat, '⚠️ Debes mencionar a un usuario para expulsarlo.', m);
     }
 
-    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
+    let user = m.mentionedJid[0] || m.quoted.sender;
+    let groupInfo = await conn.groupMetadata(m.chat);
+    let ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net';
+    let ownerBot = global.owner[0][0] + '@s.whatsapp.net';
 
-    const groupInfo = await conn.groupMetadata(m.chat);
-    const ownerGroup = groupInfo.owner || m.chat.split`-`[0] + '@s.whatsapp.net';
-    const ownerBot = global.owner[0][0] + '@s.whatsapp.net';
-    //const nn = conn.getName(m.sender);
-
-    if (user === conn.user.jid) {
-        return conn.reply(m.chat, `${emoji2} No puedo eliminar el bot del grupo.`, m);
+    if ([conn.user.jid, ownerGroup, ownerBot].includes(user)) {
+        return conn.reply(m.chat, '⚠️ No puedes eliminar a este usuario.', m);
     }
 
-    if (user === ownerGroup) {
-        return conn.reply(m.chat, `${emoji2} No puedo eliminar al propietario del grupo.`, m);
+    try {
+        await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
+    } catch (e) {
+        return conn.reply(m.chat, '❌ No se pudo eliminar al usuario. Asegúrate de que el bot es administrador.', m);
     }
-
-    if (user === ownerBot) {
-        return conn.reply(m.chat, `${emoji2} No puedo eliminar al propietario del bot.`, m);
-    }
-
-    await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
-
-//conn.reply(`${suitag}@s.whatsapp.net`, `${emoji} Un Admin Acabo De Eliminar Un Usuario En El Grupo:\n> ${groupMetadata.subject}.`, m, rcanal, );
 };
 
 handler.help = ['kick'];
 handler.tags = ['grupo'];
-handler.command = ['kick','echar','hechar','sacar','ban'];
+handler.command = ['kick', 'echar', 'sacar', 'ban'];
 handler.admin = true;
 handler.group = true;
-handler.register = true
 handler.botAdmin = true;
 
 export default handler;
