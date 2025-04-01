6c8b1932e47afa72b9f763d4ce2405e6d806dfac
Aqua200 <chinquepapa@gmail.com>
2025-04-01 12:52:24 -0700

    Update owner-bcgc.js

--- plugins/owner-bcgc.js
+++ plugins/owner-bcgc.js
@@ -2,7 +2,7 @@ const handler = async (m, { conn, isROwner, text, args }) => { const delay = (ti
 
 const pesan = m.quoted && m.quoted.text ? m.quoted.text : text; if (!pesan) throw ${emoji} ✨ 𝐸𝓈𝒸𝓇𝒾𝒷𝑒 𝒶𝓁𝑔𝑜 𝓅𝒶𝓇𝒶 𝑒𝓃𝓋𝒾𝒶𝓇 ✨;
 
-const imageUrl = args[1] || "https://qu.ax/iKouo.jpeg"; // URL por defecto si no se proporciona una const kawaiiBorder = "━━━━━━━━━━━━━━━━━━━━";
+const imageUrl = args[1] || "https://files.catbox.moe/itgvo5.jpeg"; // URL por defecto si no se proporciona una const kawaiiBorder = "━━━━━━━━━━━━━━━━━━━━";
 
 for (const i of anu) { await delay(500); const caption = 🌸 𝒦𝒶𝓌𝒶𝒾𝒾 𝑀𝑒𝓃𝓈𝒶𝒿𝑒 🌸\n${kawaiiBorder}\n${pesan}\n${kawaiiBorder}\n✨ 𝒫𝑜𝓇: ${packname} ✨; conn.sendMessage(i, { image: { url: imageUrl }, caption }); }
 
