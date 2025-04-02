720ec5014e99b4fe54a54312235530b026b3bd46
Aqua200 <chinquepapa@gmail.com>
2025-04-02 10:50:21 -0700

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,14 +1,19 @@
+const gp1 = 'https://chat.whatsapp.com/Lf5b8iMlbCbEvYlDQC6SgC'; // Reemplaza con tu enlace
+
 export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
-  if (m.isBaileys && m.fromMe) return !0;
-  if (m.isGroup) return !1;
-  if (!m.message) return !0;
-  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
-  const chat = global.db.data.chats[m.chat];
+  if (m.isBaileys && m.fromMe) return true;
+  if (m.isGroup) return false;
+  if (!m.message) return true;
+  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return true;
+  
   const bot = global.db.data.settings[this.user.jid] || {};
-if (m.chat === '120363322713003916@newsletter') return !0
+
+  if (m.chat === '120363322713003916@newsletter') return true;
+
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`${emoji} Hola @${m.sender.split`@`[0]}, mi creador a desactivado los comandos en los chats privados el cual serás bloqueado, si quieres usar los comandos del bot te invito a que te unas al grupo principal del bot.\n\n${gp1}`, false, {mentions: [m.sender]});
+    await m.reply(`⚠️ Hola @${m.sender.split`@`[0]}, mi creador ha desactivado los comandos en los chats privados, por lo que serás bloqueado. Si quieres usar los comandos del bot, únete al grupo principal:\n\n${gp1}`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
-  return !1;
+  
+  return false;
 }
