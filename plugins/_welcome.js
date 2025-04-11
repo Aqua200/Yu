240446e18ca3693fe8733b982b327a75b01a7ad1
Aqua200 <chinquepapa@gmail.com>
2025-04-11 15:50:50 -0700

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -4,8 +4,6 @@ export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
   
   let chat = global.db.data.chats[m.chat]
-  let txt = 'ゲ◜៹ New Member ៹◞ゲ'
-  let txt1 = 'ゲ◜៹ Bye Member ៹◞ゲ'
   let groupSize = participants.length
   
   if (m.messageStubType == 27) {
@@ -15,7 +13,23 @@ export async function before(m, { conn, participants, groupMetadata }) {
   }
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `❀ *Bienvenido* a ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`    
+    let user = m.messageStubParameters[0].split('@')[0]
+    let bienvenida = `
+✦═════════════✦  
+   🤍 *BIENVENIDO(A)* 🤍  
+✦═════════════✦  
+
+   ✧ @${user}  
+   ✧ Al grupo: *${groupMetadata.subject}*  
+   ✧ Miembros: *${groupSize}*  
+
+   💜 *¡Disfruta tu estadía!*  
+   📜 *Normas:* Respeto y buen humor.  
+
+✦═════════════✦  
+   🔹 Usa *#menu* para ver comandos  
+✦═════════════✦  
+    `
     await conn.sendMessage(m.chat, { 
       text: bienvenida, 
       mentions: [m.messageStubParameters[0]] 
@@ -23,9 +37,24 @@ export async function before(m, { conn, participants, groupMetadata }) {
   }
   
   if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
-    let bye = `❀ *Adiós* de ${groupMetadata.subject}\n✰ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom2}\n✦ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+    let user = m.messageStubParameters[0].split('@')[0]
+    let despedida = `
+✦═════════════✦  
+   🖤 *ADIÓS* 🖤  
+✦═════════════✦  
+
+   ✧ @${user}  
+   ✧ Ha dejado: *${groupMetadata.subject}*  
+   ✧ Miembros restantes: *${groupSize}*  
+
+   🌟 *¡Te esperamos de vuelta!*  
+
+✦═════════════✦  
+   🔸 Gracias por tu participación  
+✦═════════════✦  
+    `
     await conn.sendMessage(m.chat, { 
-      text: bye, 
+      text: despedida, 
       mentions: [m.messageStubParameters[0]] 
     })
   }
