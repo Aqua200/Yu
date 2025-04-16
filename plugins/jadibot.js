5ac18977a24b191d0c2790d58a5c58ba8e2296b5
Aqua200 <chinquepapa@gmail.com>
2025-04-16 01:37:01 -0700

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -45,27 +45,19 @@ case isCommand3:
 //if (global.db.data.settings[conn.user.jid].jadibotmd) return m.reply(`${emoji} Este comando está desactivado por mi creador.`)
 const users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
 function convertirMsADiasHorasMinutosSegundos(ms) {
-var segundos = Math.floor(ms / 1000);
-var minutos = Math.floor(segundos / 60);
-var horas = Math.floor(minutos / 60);
-var días = Math.floor(horas / 24);
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
-var resultado = "";
-if (días !== 0) {
-resultado += días + " días, ";
-}
-if (horas !== 0) {
-resultado += horas + " horas, ";
-}
-if (minutos !== 0) {
-resultado += minutos + " minutos, ";
-}
-if (segundos !== 0) {
-resultado += segundos + " segundos";
-}
-return resultado;
+        const segundos = Math.floor(ms / 1000)
+        const minutos = Math.floor(segundos / 60)
+        const horas = Math.floor(minutos / 60)
+        const días = Math.floor(horas / 24)
+        const segRest = segundos % 60
+        const minRest = minutos % 60
+        const horasRest = horas % 24
+        let resultado = ""
+        if (días !== 0) resultado += `${días} días, `
+        if (horasRest !== 0) resultado += `${horasRest} horas, `
+        if (minRest !== 0) resultado += `${minRest} minutos, `
+        if (segRest !== 0) resultado += `${segRest} segundos`
+        return resultado.trim()
 }
 const message = users.map((v, index) => `• 「 ${index + 1} 」\n📎 Wa.me/${v.user.jid.replace(/[^0-9]/g, '')}?text=${usedPrefix}estado\n👤 Usuario: ${v.user.name || 'Sub-Bot'}\n🕑 Online: ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : 'Desconocido'}`).join('\n\n__________________________\n\n');
 const replyMessage = message.length === 0 ? `No hay Sub-Bots disponible por el momento, verifique mas tarde.` : message;
